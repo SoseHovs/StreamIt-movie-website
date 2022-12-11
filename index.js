6813994d87bbada612af58a0afefad6d3a100078
SoseHovs <sose.hovsepyan1996@gmail.com>
2022-12-11 16:21:37 +0400

    swap slides chnages

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-// const slider = document.querySelectorAll('.slider_list');
-// const next = document.querySelectorAll('.next');
-// const prev = document.querySelectorAll('.prev');
-// let direction = 0;
+const slider = document.querySelectorAll('.slider_list');
+const next = document.querySelectorAll('.next');
+const prev = document.querySelectorAll('.prev');
+let direction = 0;
 
-// let width=slider[1].children[1].offsetWidth
-// let style= getComputedStyle(slider[1].children[1]);
-// let itemMarginRight= style.marginRight;
-// let itemMarginLeft=style.marginLeft
-// let margins=parseInt(itemMarginRight)+parseInt(itemMarginLeft)
-// let fullWidth = width+margins
-
-
+let width=slider[1].children[1].offsetWidth
+let style= getComputedStyle(slider[1].children[1]);
+let itemMarginRight= style.marginRight;
+let itemMarginLeft=style.marginLeft
+let margins=parseInt(itemMarginRight)+parseInt(itemMarginLeft)
+let fullWidth = width+margins
 
 
-// prev.forEach((prevs, index) =>  prevs.addEventListener ('click', function () {
-//     direction = -1;
-//     slider[index].style.transform = "translate(" + fullWidth + "px)"
-// })   
-//     )
-
-// next.forEach((nexts,index) => nexts.addEventListener ('click', function () {
-//     direction = 1
-//     slider[index].style.transform = "translate(" + (-fullWidth) +"px)"
-// })    )
-
-// slider.forEach(sliders =>  sliders.addEventListener ('transitionend', function () {
-//     if (direction === 1) {
-//         sliders.appendChild (sliders.firstElementChild);
-//     } else if (direction === -1) {
-//         sliders.prepend (sliders.lastElementChild);
-//     }
-//     sliders.style.transition = 'none';
-//     sliders.style.transform = 'translate(0)';
-//     setTimeout (() => {
-//         sliders.style.transition = 'all 1s';
-//         direction = 0
-//     })
-// }, false))
 
 
-// let sideBar = document.querySelector(".sidebar")
-// let menuLogo = document.querySelector(".menu_logo")
+prev.forEach((prevs, index) =>  prevs.addEventListener ('click', function () {
+    direction = -1;
+    slider[index].style.transform = "translate(" + fullWidth + "px)"
+})   
+    )
 
-// menuLogo.addEventListener("click", function(){
+next.forEach((nexts,index) => nexts.addEventListener ('click', function () {
+    direction = 1
+    slider[index].style.transform = "translate(" + (-fullWidth) +"px)"
+})    )
 
-// sideBar.classList.toggle("sidebarShow")
-// sideBar.style.transition="1s"
+slider.forEach(sliders =>  sliders.addEventListener ('transitionend', function () {
+    if (direction === 1) {
+        sliders.appendChild (sliders.firstElementChild);
+    } else if (direction === -1) {
+        sliders.prepend (sliders.lastElementChild);
+    }
+    sliders.style.transition = 'none';
+    sliders.style.transform = 'translate(0)';
+    setTimeout (() => {
+        sliders.style.transition = 'all 1s';
+        direction = 0
+    })
+}, false))
 
 
-// })
+let sideBar = document.querySelector(".sidebar")
+let menuLogo = document.querySelector(".menu_logo")
+
+menuLogo.addEventListener("click", function(){
+
+sideBar.classList.toggle("sidebarShow")
+sideBar.style.transition="1s"
+
+
+})
 
 
 let trendingMovies=document.querySelector(".trend_movies_list"),
@@ -63,10 +63,6 @@ trendingMovies.addEventListener("click", (event)=>{
     trendingMovies.insertBefore(clonned,event.target)
       centerContainer.appendChild(event.target)
     centerContainerMovie.remove()   
-
-   
-
-
 
 
 
