d8c3cb30cc721ec4fff2ee063fc085644396e637
SoseHovs <sose.hovsepyan1996@gmail.com>
2023-01-02 11:04:52 +0400

    changes

--- index.js
+++ index.js
@@ -52,18 +52,106 @@ sideBar.style.transition="1s"
 
 
 let trendingMovies=document.querySelector(".trend_movies_list"),
-centerContainer=document.querySelector(".active_trend"),
-centerContainerMovie;
+    centerContainer=document.querySelector(".active_trend"),
+    centerContainerMovie,
+    moviesList=document.querySelectorAll(".trend_movies_list img"),
+    rankingNumber=document.querySelector(".topTrends_banner .rankingNumber"),
+    actorsList=document.querySelector(".starringActors"),
+    movieGenre=document.querySelector(".movieGenre"),
+    movieTags=document.querySelector(".movieTags"),
+    topTrends=document.querySelector(".topTrends_banner"),
+    TrendMovieList = [
+    {movieName:"The Mandalorian",
+     topRanking:"5",
+     description:"The travels of a lone bounty hunter in the outer reaches if the galaxy, far from the authority of the new republic",
+     starring:"James Earl Jones, John Khani",
+     genres:"action, crime",
+     tags:"music, mythology",
+    imageSource:'img/banner-2eye.jpg'},
+
+     {movieName:"The reckless",
+     topRanking:"2",
+     description:"The travels of a lone bounty hunter in the outer reaches if the galaxy, far from the authority of the new republic",
+     starring:"James Earl Jones,James Chinlund",
+     genres:"Drama,Fantasy",
+     tags:"Brother, Brother Relationship, Kings, Vikings",
+     imageSource:'img/banner-4.jpg'},
+
+     {movieName:"Zombie World",
+     topRanking:"1",
+     description:"The travels of a lone bounty hunter in the outer reaches if the galaxy, far from the authority of the new republic",
+     starring:"Debbi Bossi, Karen Gilshrist",
+     genres:"Action, adventure, drama",
+     tags:"Brother relationship, king, apocalypse",
+     imageSource:'img/banner-3zombie-island-banner-1.jpg' },
+
+     {movieName:"Criminals and Agents",
+     topRanking:"3",
+     description:"The travels of a lone bounty hunter in the outer reaches if the galaxy, far from the authority of the new republic",
+     starring:"Debbi Rossi, Mark Livolsi",
+     genres:"Drama,Romance",
+     tags:"Brothers, criminal, realtionships",
+     imageSource:'img/Banner-5.jpg' },
+
+     {movieName:"Football",
+     topRanking: "6",
+     description:"The travels of a lone bounty hunter in the outer reaches if the galaxy, far from the authority of the new republic",
+     starring:"Robert Rodriguez, Robert Romanson",
+     genres:"Comedy, drama",
+     tags:"4k ultra, brother, relationship, vikings",
+     imageSource:'img/banner-1.jpg'},
+]
+
 
 trendingMovies.addEventListener("click", (event)=>{
+  let centerContainerMovie=document.querySelectorAll(".active_trend img")[0]
+ 
+    
+    if(event.target.getAttribute('src')===centerContainerMovie.getAttribute('src')){
+            return;
+    } 
 
-    centerContainerMovie=document.querySelectorAll(".active_trend img")[0]
-    let clonned=centerContainerMovie.cloneNode(true)
-    trendingMovies.appendChild(clonned)
-    trendingMovies.insertBefore(clonned,event.target)
-      centerContainer.appendChild(event.target)
-    centerContainerMovie.remove()   
+    else{
+        let clonned=centerContainerMovie.cloneNode(true)
+        trendingMovies.appendChild(clonned)
+        trendingMovies.insertBefore(clonned,event.target)
+        centerContainer.appendChild(event.target)
+        centerContainerMovie.remove()
 
+
+        topTrends.style.opacity="0"
+        topTrends.style.transition="1s"
+
+
+    topTrends.addEventListener('transitionend',()=>{
+
+        for(let i=0; i<TrendMovieList.length; i++){
+            if(event.target.getAttribute('src')===TrendMovieList[i].imageSource){
+                topTrends.style.opacity="1"
+                topTrends.style.backgroundImage="linear-gradient(to right, rgb(0 0 0 / 93%), rgba(0, 0, 0, 0) ),url("+TrendMovieList[i].imageSource+")"
+                rankingNumber.innerHTML= TrendMovieList[i].topRanking
+                actorsList.innerHTML= TrendMovieList[i].starring
+                movieGenre.innerHTML= TrendMovieList[i].genres
+                movieTags.innerHTML= TrendMovieList[i].tags
+
+            }
+    }
+
+    })
+
+}
+})
+
+
+let topMovies=document.querySelectorAll(".top_movies_item img")
+let topBanner= document.querySelector(".us_top_movies")
+topMovies.forEach((item) =>{
+
+item.addEventListener('click', (event)=>{
+    let movieItem=event.target.getAttribute('src')
+    topBanner.style.backgroundImage= "url(" +movieItem+")"
+ 
+})
 
 
 })
